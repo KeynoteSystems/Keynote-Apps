cabfe2dcca6fe28ced3f8def879084ba16b958ee
Robert Castley <robert.castley@gmail.com>
2013-07-03 12:58:09 +0100

    Code tidy

--- API-Showcase/application/controllers/ReportController.php
+++ API-Showcase/application/controllers/ReportController.php
@@ -1,40 +1,40 @@
 <?php
 class ReportController extends Zend_Controller_Action
 {
-    /**
-     * Session
-     *
-     * @var array
-     */
-    private $_session;
+	/**
+	 * Session
+	 *
+	 * @var array
+	 */
+	private $_session;
 
-    public function init()
-    {
+	public function init()
+	{
 
-    }
+	}
 
-    public function indexAction()
-    {
-        $config = Zend_Registry::get('config');
+	public function indexAction()
+	{
+		$config = Zend_Registry::get('config');
 
-        $this->_session = new Zend_Session_Namespace('DASHBOARD');
+		$this->_session = new Zend_Session_Namespace('DASHBOARD');
 
-        $url = Zend_Controller_Front::getInstance()->getRequest()->getRequestUri();
+		$url = Zend_Controller_Front::getInstance()->getRequest()->getRequestUri();
 
-        $this->_session->url = $url;
+		$this->_session->url = $url;
 
-        $api = new Keynote_Client();
+		$api = new Keynote_Client();
 
-        if ($this->_session->apiKey) {
-            $api->api_key = $this->_session->apiKey;
-        } else {
-            $this->_redirect('index');
-        }
+		if ($this->_session->apiKey) {
+			$api->api_key = $this->_session->apiKey;
+		} else {
+			$this->_redirect('index');
+		}
 
-        $this->view->data = $api->getActiveSlotMetaData();
+		$this->view->data = $api->getActiveSlotMetaData();
 
-        $this->view->alarm = $api->getAlarmMetaData();
+		$this->view->alarm = $api->getAlarmMetaData();
 
-        $this->view->cDate = date('Y-m-d H:i:s');
-    }
+		$this->view->cDate = date('Y-m-d H:i:s');
+	}
 }
