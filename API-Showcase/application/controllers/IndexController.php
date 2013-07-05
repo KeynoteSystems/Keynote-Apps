80f4132902802f9f6293c150882e07bdd5b5ecc0
Robert Castley <robert.castley@gmail.com>
2013-07-05 10:10:15 +0100

    Code tidy

--- API-Showcase/application/controllers/IndexController.php
+++ API-Showcase/application/controllers/IndexController.php
@@ -1,25 +1,25 @@
 <?php
 class IndexController extends Zend_Controller_Action
 {
-    /**
-     * Session
-     *
-     * @var array
-     */
-    private $_session = array();
+	/**
+	 * Session
+	 *
+	 * @var array
+	 */
+	private $_session = array();
 
-    public function indexAction()
-    {
-        $this->view->config = Zend_Registry::get('config');
+	public function indexAction()
+	{
+		$this->view->config = Zend_Registry::get('config');
 
-        $this->_session = new Zend_Session_Namespace('DASHBOARD');
+		$this->_session = new Zend_Session_Namespace('DASHBOARD');
 
-        if (!isset($this->_session->apiKey)) {
-            if($this->view->config['apikey'] != null) {
-                $this->_session->apiKey = $this->view->config['apikey'];
-            }
-        }
+		if (!isset($this->_session->apiKey)) {
+			if($this->view->config['apikey'] != null) {
+				$this->_session->apiKey = $this->view->config['apikey'];
+			}
+		}
 
-        $this->view->api_key = $this->_session->apiKey;
-    }
+		$this->view->api_key = $this->_session->apiKey;
+	}
 }
