7e633b3c7ec2ad082648be0cc3703fed7d886b1b
Robert Castley <robert.castley@gmail.com>
2013-07-05 10:09:53 +0100

    Store slotData in session after saving API key

--- API-Showcase/application/controllers/SaveController.php
+++ API-Showcase/application/controllers/SaveController.php
@@ -20,12 +20,23 @@ class SaveController extends Zend_Controller_Action
 
 		$this->_session = new Zend_Session_Namespace('DASHBOARD');
 
-		$api_key = $this->_getParam('api_key');
+		$this->_session->apiKey = $this->_getParam('api_key');
 
-		$this->_session->apiKey = $api_key;
+		$api = new Keynote_Client();
 
-		$this->_redirect($this->_session->url);
+		$api->api_key = $this->_session->apiKey;
 
+		$this->_session->slotData = $api->getActiveSlotMetaData();
+
+		$frontendOptions = array();
+
+		$backendOptions = array('cache_dir' => '../data/cache');
+
+		$cache = Zend_Cache::factory('Page', 'File', $frontendOptions, $backendOptions);
+
+		$cache->clean(Zend_Cache::CLEANING_MODE_ALL);
+
+		//$this->_redirect($this->_session->url);
 	}
 
 }
