935940aca179b9af033a0f34325d073e59f88ba7
Robert Castley <robert.castley@gmail.com>
2013-11-08 14:19:49 +0000

    Support for more datapoints on Scatterplot

--- API-Showcase/application/controllers/GraphController.php
+++ API-Showcase/application/controllers/GraphController.php
@@ -65,7 +65,7 @@ class GraphController extends Zend_Controller_Action
 		if ($this->_request->getParam('graphType') == 'scatter') {
 			//$nDays = 3600;
 			$nDays = $this->_request->getParam('Days');
-			//$bSize = 300;
+			$bSize = 300;
 			$basePageOnly = 'true';
 		} else {
 			$nDays = $this->_request->getParam('Days');
