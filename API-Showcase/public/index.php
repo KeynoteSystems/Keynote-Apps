f02c49b585fe3382154267adda823b30d2807f47
rcastley <robert.castley@keynote.com>
2014-01-17 21:47:18 +0000

    Formatting changes

--- API-Showcase/public/index.php
+++ API-Showcase/public/index.php
@@ -1,12 +1,15 @@
 <?php
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+defined ( 'APPLICATION_PATH' ) || define ( 'APPLICATION_PATH', realpath ( dirname ( __FILE__ ) . '/../application' ) );
 
-defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+defined ( 'APPLICATION_ENV' ) || define ( 'APPLICATION_ENV', (getenv ( 'APPLICATION_ENV' ) ? getenv ( 'APPLICATION_ENV' ) : 'production') );
 
-set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'), get_include_path())));
+set_include_path ( implode ( PATH_SEPARATOR, array (
+		realpath ( APPLICATION_PATH . '/../library' ),
+		get_include_path () 
+) ) );
 
 require_once 'Zend/Application.php';
 
-$application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/config/application.ini');
+$application = new Zend_Application ( APPLICATION_ENV, APPLICATION_PATH . '/config/application.ini' );
 
-$application->bootstrap()->run();
+$application->bootstrap ()->run ();
